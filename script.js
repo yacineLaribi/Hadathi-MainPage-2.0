8530ca895367361200d0f60d8b8d1e41c4bbc9b7
Yacine Laribi <128645439+yacineLaribi@users.noreply.github.com>
2023-05-26 21:51:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+   
+  }
+}
